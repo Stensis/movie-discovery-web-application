12da7c4825f73c5a40b98216c0b68e13d74ddf8e
Stensis <irenenjuguna98@gmail.com>
2023-09-12 03:31:11 +0300

    added movie card functionality and homepage

--- src/Components/MovieCard/movieCard.js
+++ src/Components/MovieCard/movieCard.js
@@ -7,25 +7,24 @@ function MovieCard({ movie }) {
   return (
     <div className="card" style={{ width: "18rem" }} data-testid="movie-card">
       <img
-        src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_640.jpg"
-        alt={movie.title || 'Movie'}
+        src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
+        alt={movie.title || "Movie"}
         className="card-img-top movie-poster"
         data-testid="movie-poster"
       />
       <div className="card-body">
         <h3 className="card-title movie-title" data-testid="movie-title">
-          {movie.title || 'Title not available'}
+          {movie.title || "Title not available"}
         </h3>
         <p
           className="card-text movie-release-date"
           data-testid="movie-release-date"
         >
-          {movie.releaseDate || 'Release date not available'}
+          {movie.release_date || "Release date not available"}
         </p>
       </div>
     </div>
   );
 }
-
 
 export default MovieCard;
