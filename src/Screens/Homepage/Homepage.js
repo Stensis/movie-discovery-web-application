c82392d25fcd58cdbf710715060a757a3a744d0d
Stensis <irenenjuguna98@gmail.com>
2023-09-12 03:44:58 +0300

    added movie card functionality and homepage

--- src/Screens/Homepage/Homepage.js
+++ src/Screens/Homepage/Homepage.js
@@ -3,6 +3,8 @@ import Navbar from "./Navbar/Navbar";
 import MovieCard from "../../Components/MovieCard/movieCard";
 import Footer from "../Footer/Footer";
 
+console.log(`Bearer ${process.env.REACT_APP_BEARER_TOKEN}`);
+
 export default function Homepage() {
   const [allMovies, setAllMovies] = useState([]);
 
@@ -14,11 +16,29 @@ export default function Homepage() {
         Authorization: `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`,
       },
     };
+    
+
     fetch("https://api.themoviedb.org/3/trending/movie/day", options)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((data) => {
-        const slicedArray = data.results.slice(0, 10);
-        setAllMovies(slicedArray);
+        if (data && data.results) {
+          const slicedArray = data.results.slice(0, 10);
+          setAllMovies(slicedArray);
+        } else {
+          console.error("Unexpected API response:", data);
+        }
+      })
+
+      .catch((error) => {
+        console.error(
+          "There was a problem with the fetch operation:",
+          error.message
+        );
       });
   }, []);
 
